51bd8fb56bf7b42c9bca8dccb630d43331c1fd48
RickCarr <rick.carr@telus.com>
2023-01-24 01:31:07 -0800

    creating 3 stores in file exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,26 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+b = Store.new
+b.name = "Burnaby"
+b.annual_revenue = 300000
+b.mens_apparel = true
+b.womens_apparel = true
+b.save
+
+r = Store.new
+r.name = "Richmond"
+r.annual_revenue = 1260000
+r.mens_apparel = false
+r.womens_apparel = true
+r.save
+
+g = Store.new
+g.name = "Gastown"
+g.annual_revenue = 190000
+g.mens_apparel = true
+g.womens_apparel = false
+g.save
+
+puts Store.count
