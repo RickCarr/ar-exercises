58962621fedffe3f5e944a0c289be1b37fab31fb
RickCarr <rick.carr@telus.com>
2023-01-24 01:34:25 -0800

    creating 3 stores and creating instance variables for mens_stores and womens_stores in file exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,32 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+s = Store.new
+s.name = "Surrey"
+s.annual_revenue = 224000
+s.mens_apparel = false
+s.womens_apparel = true
+s.save
+
+w = Store.new
+w.name = "Whistler"
+w.annual_revenue = 1900000
+w.mens_apparel = true
+w.womens_apparel = false
+w.save
+
+y = Store.new
+y.name = "Yaletown"
+y.annual_revenue = 430000
+y.mens_apparel = true
+y.womens_apparel = true
+y.save
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "#{store.name} #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < 1000000")
+
