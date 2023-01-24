b7e0a7d2c68fc6af96cc18239a685d2eabebb5bd
RickCarr <rick.carr@telus.com>
2023-01-24 01:38:48 -0800

    adding one to many association and 3 employees for store1 and store2

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,19 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Kuai", last_name: "Liang", hourly_rate: 32)
+@store1.employees.create(first_name: "Hanzo", last_name: "Hasashi", hourly_rate: 187)
+@store1.employees.create(first_name: "Takeda", last_name: "Takahashi", hourly_rate: 99)
+
+@store2.employees.create(first_name: "Ryan", last_name: "Montgomery", hourly_rate: 59)
+@store2.employees.create(first_name: "Marshall", last_name: "Mathers", hourly_rate: 88)
+@store2.employees.create(first_name: "Kendrick", last_name: "Lamar", hourly_rate: 80)
+
