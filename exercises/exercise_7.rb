79e233e50dcdb6c9394bdcf7abbc74eface0a01f
RickCarr <rick.carr@telus.com>
2023-01-24 01:40:45 -0800

    adding validations for Employee and Stores. and testing via user input with return error messages

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,31 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true}
+  validates :hourly_rate, inclusion: {in: 40..200}
+  validates :store, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {only_integer: true}
+  validates :annual_revenue, numericality: {minimum: 0}
+end
+
+puts "Enter the NAME of a store"
+user_store = gets.chomp
+
+new_store = Store.new(name: user_store)
+
+if new_store.save
+  puts "Saved."
+else
+  puts "Oh no! Error detected:"
+  new_store.errors.full_messages.each do |message|
+    puts "#{message}"
+  end
+end
+
